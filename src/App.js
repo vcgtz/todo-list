f2f2883a346373eb75979ad80a7dfc972218ad7a
Vicente Gutiérrez <gtz.rvera@gmail.com>
2022-06-14 22:00:04 -0500

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { render } from 'react-dom';
 import Sidebar from './Sidebar.jsx';
 import NotesList from './NotesList.jsx';
+import Header from './Header.jsx';
 
 const App = () => (
-  <div className="flex">
-    <div className='w-3/12'>
-      <Sidebar />
-    </div>
-    <div className='w-9/12'>
-      <NotesList />
+  <div>
+    <Header />
+
+    <div className="flex">
+      <div className='w-3/12'>
+        <Sidebar />
+      </div>
+      <div className='w-9/12'>
+        <NotesList />
+      </div>
     </div>
   </div>
 );
